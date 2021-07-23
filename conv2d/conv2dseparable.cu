57a6a3353cb310d3f4fbc4eb5dbf2a6cc1023e96
AhmedAmraniAkdi <ahmed-trabajos@hotmail.com>
2021-07-23 17:13:31 +0100

    grid and block dims

--- conv2d/conv2dseparable.cu
+++ conv2d/conv2dseparable.cu
@@ -7,17 +7,32 @@
 #define IMCOLS 1880 
 #define IMCHANNELS 1
 #define KERNELRADIUS 16 // 16x2 + 1
-#define BLOCKSIZE 16
+
+#define BLOCKDIM_1 16
+#define BLOCKDIM_2 4
+#define STEP 4
 
 __constant__ float c_kernel[KERNELRADIUS * 2 + 1];
 
-__global__ void conv2d(float *d_input, float *d_output){
+__global__ void conv2d_row(float *d_input, float *d_output){
+
+    int idx_x = blockDim.x * blockIdx.x + threadIdx.x;
+    int idx_y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    __shared__ s_data[];
 
 };
 
+__global__ void conv2d_col(float *d_input, float *d_output){
+
+
+
+};
+
+// add err checking
 void processing(float* h_input, float *h_output, float *h_kernel){
     // variables for device
-    float *d_input,*d_output, *d_kernel;
+    float *d_input, *d_intermediate_output, *d_output, *d_kernel;
     int buf_size = IMROWS * IMCOLS * sizeof(float);
     int kernel_size = (KERNELRADIUS*2+1) * sizeof(float);
 
@@ -26,27 +41,49 @@ void processing(float* h_input, float *h_output, float *h_kernel){
 
     // allocate device mem
     cudaMalloc((void **)&d_input, buf_size);
+    cudaMalloc((void **)&d_intermediate_output, buf_size);
     cudaMalloc((void **)&d_output, buf_size);
 
     // send data
     cudaMemcpy(d_input, h_input, buf_size, cudaMemcpyHostToDevice);
     cudaMemcpyToSymbol(c_kernel, h_kernel, kernel_size);
 
+    // dimensions
+
+    dim3 dimBlock_row(BLOCKDIM_1, BLOCKDIM_2);
+    int temp = STEP * BLOCKDIM_1;
+    dim3 dimGrid_row((IMCOLS + temp - 1)/temp, (IMROWS + BLOCKDIM_2 - 1)/BLOCKDIM_2);
+
     // where magic happens
+    // row
     sdkStartTimer(&timer);
     cudaProfilerStart();
-    conv2d(d_input, d_output);
-    cudaDeviceSynchronize();
-    sdkStopTimer(&timer);
+    conv2d_row<<<dimGrid_row, dimBlock_row>>>(d_input, d_intermediate_output);
     cudaProfilerStop();
+    sdkStopTimer(&timer);
+    printf("Processing Time: %.2f ms\n", sdkGetTimerValue(&timer));
+
+    dim3 dimBlock_col(BLOCKDIM_2, BLOCKDIM_1);
+    int temp = STEP * BLOCKDIM_2;
+    dim3 dimGrid_col((IMCOLS + BLOCKDIM_1 - 1)/BLOCKDIM_1, (IMROWS + temp - 1)/temp);
+    
+    //col
+    sdkResetTimer(&timer);
+    sdkStartTimer(&timer);
+    cudaProfilerStart();
+    conv2d_col<<<dimGrid_col, dimBlock_col>>>(d_intermediate_output, d_output);
+    cudaProfilerStop();
+    sdkStopTimer(&timer);
     printf("Processing Time: %.2f ms\n", sdkGetTimerValue(&timer));
     
+    cudaDeviceSynchronize();
+
     // return data
     cudaMemcpy(h_output, d_output, buf_size, cudaMemcpyDeviceToHost);
 
     // free
-    free(d_input);
-    free(d_output);
-
+    cudaFree(d_input);
+    cudaFree(d_output);
+    cudaFree(d_intermediate_output);
 
 }
