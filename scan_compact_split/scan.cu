4e01a16b6d744ae76851f1c0701b0571822d01fc
AhmedAmraniAkdi <ahmed-trabajos@hotmail.com>
2021-08-04 23:03:51 +0100

    going to use float4, keep it simple, see results, optimise

--- scan_compact_split/scan.cu
+++ scan_compact_split/scan.cu
@@ -6,26 +6,67 @@
 
 __device__ void sweeps(float *d_input, float *intermediate_sum, float *d_output, int step){
 
+    __shared__ float s_data[BLOCKDIM * STEP];
+
+    int idx = blockDim.x * blockIdx.x * STEP + threadIdx.x;
+
+    d_input += idx;
+    d_output += idx;
+
+    #pragma unroll
+    for(int i = 0; i < STEP; i++){
+        s_data[i * BLOCKDIM + threadIdx.x] = d_input[i * BLOCKDIM];
+    }
+
+    __syncthreads();
+
+    /* each thread working 4 times (STEP = 4)
+
+        |512|512|512|512|
+
+        after the up sweep, we will have to sum each sub block last element, 
+        which will add log2STEP iterations.
+
+        first loop is for the subblocks of 512 (9 iterations)
+
+        second loop is for the 2 last iterations
+
+        total is 11 (log2 (4*512))
+    */
+    #pragma unroll
+    for(int offset = 1; offset < LOG2_BLOCKSIZE; offset <<= 1){
+
+        #pragma unroll
+        for(int i = 0; i < STEP; i++){
+            
+        }
+
+        __syncthreads();
+    }
+
 }
+
 
 __device__ void scanBlockSums(float *d_input, float *d_output, int step){
 
 }
 
+
 __device__ void sumScannedSum(float *scanned_sum, float *output, int step){
 
 }
 
-void scan(float* d_input, float* d_output, int arr_size){
 
-    int temp = (arr_size/sizeof(float) + STEP * BLOCKDIM - 1)/STEP/BLOCKDIM;
+void scan(float4* d_input, float4* d_output, int arr_size){
+
+    int temp = (arr_size + BLOCKDIM - 1)/BLOCKDIM;
     dim3 dimBlock(BLOCKDIM);
     dim3 dimGrid(temp);
 
     float *intermediate_sum;
     float *intermediate_sum_scanned;
-    cudaMalloc((void **)&intermediate_sum, arr_size);
-    cudaMalloc((void **)&intermediate_sum_scanned, arr_size);
+    cudaMalloc((void **)&intermediate_sum, arr_size * sizeof(float));
+    cudaMalloc((void **)&intermediate_sum_scanned, arr_size * sizeof(float));
 
     sweeps<<<dimGrid, dimBlock>>>(d_input, intermediate_sum, d_output, STEP);
     cudaDeviceSynchronize();
@@ -42,24 +83,32 @@ void scan(float* d_input, float* d_output, int arr_size){
     cudaFree(intermediate_sum_scanned);
 }   
 
-void fill_array(float *h_input, int arr_size){
+
+void fill_array(float4 *h_input, int arr_size){
+
+    float *temp = (float*) h_input;
     for(int i = 0; i < arr_size; i++){
-        h_input[i] = rand() % 10;
+        temp[i] = rand() & 15;
     }
 }
 
-int check_solution(float *h_input, float * h_output, int arr_size){
-    float *temp;
-    temp = (float*) malloc(arr_size);
+
+int check_solution(float4 *h_input, float4 *h_output, int arr_size){
+    float *temp, *h_input1, *h_output1;
+
+    h_input1 = (float*)h_input;
+    h_output1 = (float*)h_output;
+
+    temp = (float*) malloc(arr_size * sizeof(float));
     
     temp[0] = 0;
-    for(int i = 1; i < arr_size/sizeof(float); i++){
-        temp[i] = temp[i - 1] + h_input[i - 1];
+    for(int i = 1; i < arr_size; i++){
+        temp[i] = temp[i - 1] + h_input1[i - 1];
     }
 
     int correct = 1;
-    for(int i = 0; i < arr_size/sizeof(float); i++){
-        if(temp[i] != h_output[i]){
+    for(int i = 0; i < arr_size; i++){
+        if(temp[i] != h_output1[i]){
             correct = 0;
             break;
         }
@@ -73,17 +122,17 @@ int main(void){
 
     srand(0);
 
-    float *h_input, *h_output;
-    float *d_input, *d_output;
-    int arr_size = SIZE_ARRAY * sizeof(float);
+    float4 *h_input, *h_output;
+    float4 *d_input, *d_output;
+    int arr_size = 1000000;
 
-    h_input = (float*) malloc(arr_size);
-    d_output = (float*) malloc(arr_size);
+    h_input = (float4*) malloc(arr_size * sizeof(float4));
+    d_output = (float4*) malloc(arr_size * sizeof(float4));
 
     fill_array(h_input, arr_size);
 
-    cudaMalloc((void **)&d_input, arr_size);
-    cudaMalloc((void **)&d_output, arr_size);
+    cudaMalloc((void **)&d_input, arr_size * sizeof(float4));
+    cudaMalloc((void **)&d_output, arr_size * sizeof(float4));
 
     cudaMemcpy(d_input, h_input, arr_size, cudaMemcpyHostToDevice);
 
--- scan_compact_split/scan_header.h
+++ scan_compact_split/scan_header.h
@@ -1,8 +1,8 @@
 #ifndef SCAN_HEADER_H
 #define SCAN_HEADER_H
 
-#define BLOCKDIM 512
-#define STEP 4
-#define SIZE_ARRAY 1000000
+#define BLOCKDIM 1024
+
+void scan(float* d_input, float* d_output, int arr_size);
 
 #endif
