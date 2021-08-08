2db7d20e04dfe6cf2e400a8f06381e9ab43f097e
AhmedAmraniAkdi <ahmed-trabajos@hotmail.com>
2021-08-08 16:54:44 +0100

    middle scan for the reduce then scan strat

--- scan/scan.cu
+++ scan/scan.cu
@@ -3,6 +3,8 @@ Reference:
     Parallel Scan for Stream Architectures1
     Duane Merrill Andrew Grimshaw
 
+inclusive scan
+
 */
 
 
@@ -11,23 +13,26 @@ Reference:
 #include <iostream>
 #include <stdlib.h>
 #include <helper_cuda.h>
+#include <cuda.h>
 
 __global__ void reduce(float4 *d_input, float *d_output){
 
     __shared__ float s_data[BLOCKDIM * 2];//1 cell per thread + another blockdim for easier indx management
 
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if(threadIdx.x == 1 && blockIdx.x == 0){
+
+    /*if(threadIdx.x == 1 && blockIdx.x == 0){
         printf("%f %f %f %f\n", d_input->w, d_input->x, d_input->y, d_input->z);
-    }
+    }*/
+
     d_input += idx;
-    d_output += idx;
+    d_output += blockIdx.x;
 
-    if(threadIdx.x == 1 && blockIdx.x == 0){
+    /*if(threadIdx.x == 1 && blockIdx.x == 0){
         printf("%f %f %f %f\n", d_input->w, d_input->x, d_input->y, d_input->z);
-    }
-
-    int sum = d_input->w + d_input->x + d_input->y + d_input->z;
+    }*/
+    float4 item = *d_input;
+    float sum = item.w + item.x + item.y + item.z;
 
     s_data[threadIdx.x] = sum;
 
@@ -55,6 +60,78 @@ __global__ void reduce(float4 *d_input, float *d_output){
     }
 }
 
+// 1 block
+__global__ void middle_scan(float *d_input, int iter_per_thread){
+
+    __shared__ float s_data[BLOCKDIM * 2];
+    
+    float seed = 0;
+
+    if(threadIdx.x == 0){
+        seed = d_input[0];
+    }
+     
+    d_input += threadIdx.x;
+
+    // cyclically scan, with the result of each scan becoming the seed to the next
+    #pragma unroll
+    for(int batch = 0; batch < iter_per_thread; batch++){
+
+        s_data[threadIdx.x] = d_input[batch * iter_per_thread];
+
+        __syncthreads();
+
+
+        //upsweep
+        float *a = s_data;
+
+        #pragma unroll
+        for(int d = LOG2_BLOCKDIM; d > 1; d--){ // we don't need last sum, inclusive scan so, the seed = first element
+
+            if( threadIdx.x < (1 << (d - 1)) ){
+
+                a[(1 << d) + threadIdx.x] = a[2 * threadIdx.x] + a[2 * threadIdx.x + 1];
+
+            }
+
+            a += (1 << d);
+            __syncthreads();
+
+        }
+
+        if(threadIdx.x == 0){
+            a[1] = a[0];
+            a[0] = seed; 
+        }
+        __syncthreads();
+
+
+        // downsweep
+        #pragma unroll
+        for(int d = 2; d <= LOG2_BLOCKDIM; d++){
+
+            a -= (1 << d);
+            
+            if( threadIdx.x < (1 << (d - 1)) ){
+
+                a[2 * threadIdx.x + 1] = a[2 * threadIdx.x] + a[(1 << d) + threadIdx.x];
+                a[2 * threadIdx.x] = a[(1 << d) + threadIdx.x];
+
+            }
+
+        __syncthreads();
+        }
+
+
+        d_input[batch * iter_per_thread] = s_data[threadIdx.x];
+
+        if(threadIdx.x == 0){
+            seed = s_data[BLOCKDIM - 1]; 
+            //printf("%f\n", seed);
+        }
+    }
+}
+
 
 void scan(float4* d_input, float4* d_output, int arr_size){
 
@@ -69,18 +146,19 @@ void scan(float4* d_input, float4* d_output, int arr_size){
     cudaDeviceSynchronize();
     checkCudaErrors(cudaGetLastError());
 
-    float *h_output = (float *) malloc(temp * sizeof(float));
+    middle_scan<<<1, dimBlock>>>(d_scan, temp/BLOCKDIM);
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
+
+    float *h_output = (float*) malloc(temp * sizeof(float));
     cudaMemcpy(h_output, d_scan, temp * sizeof(float), cudaMemcpyDeviceToHost);
 
+    std::cout << temp << "\n";
+    std::cout << temp/BLOCKDIM << "\n";
+
     for(int i = 0; i < 5; i++){
-        std::cout<<h_output[i]<< " ";
+        std::cout << h_output[i] << " ";
     }
-
-    /*scan<<<1, dimBlock>>>(d_input, d_scan, d_output);
-    cudaDeviceSynchronize();
-
-    scan<<<dimGrid, dimBlock>>>(d_input, d_scan, d_output);
-    cudaDeviceSynchronize();*/
 
     cudaFree(d_scan);
 }   
@@ -88,7 +166,7 @@ void scan(float4* d_input, float4* d_output, int arr_size){
 
 void fill_array(float4 *h_input, int arr_size){
 
-    float *temp = (float*) h_input;
+    //float *temp = (float*) h_input;
     for(int i = 0; i < arr_size/4; i++){
         //temp[i] = rand() & 15;
         //temp[i] = 1;
@@ -129,17 +207,17 @@ int main(void){
 
     srand(0);
 
+
     float4 *h_input;
     float4 *d_input;
-    int arr_size = 1 << 30;
+    int arr_size = 1 << 25;
 
-    h_input = (float4*) malloc(arr_size/4 * sizeof(float4));
+    h_input = (float4*) malloc(arr_size * sizeof(float));
 
     fill_array(h_input, arr_size);
 
-    cudaMalloc((void **)&d_input, arr_size/4 * sizeof(float4));
-
-    cudaMemcpy(d_input, h_input, arr_size/4 * sizeof(float4), cudaMemcpyHostToDevice);
+    cudaMalloc((void **)&d_input, arr_size * sizeof(float));
+    cudaMemcpy(d_input, h_input, arr_size * sizeof(float), cudaMemcpyHostToDevice);
 
     scan(d_input, NULL, arr_size);
 
